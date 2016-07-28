b5c7681e6058ec827048531e97440456e1b25be6
Hanzhiyun <hanzhiyun1995@qq.com>
2016-07-28 19:41:23 +0800

    add a private variable and add some functions to get and set the private variable

--- student.py
+++ student.py
@@ -5,16 +5,28 @@ __author__ = 'Hanzhiyun'
 
 class Student(object):
     def __init__(self, name, score):
-        self.name = name
-        self.score = score
+        self.__name = name
+        self.__score = score
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError('bad score')
 
     def print_score(self):
-        print('%s: %s' % (self.name, self.score))
+        print('%s: %s' % (self.__name, self.__score))
 
     def get_grade(self):
-        if self.score >= 90:
+        if self.__score >= 90:
             return 'A'
-        elif self.score >= 60:
+        elif self.__score >= 60:
             return 'B'
         else:
             return 'C'
