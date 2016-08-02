b66c85f89af12b3f54feadfc7124f58860d1f4d3
Hanzhiyun <hanzhiyun1995@qq.com>
2016-08-02 19:50:15 +0800

    use logging to record the error message

--- unit_debug.py
+++ unit_debug.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 __author__ = 'Hanzhiyun'
-
+import logging
+logging.basicConfig(level=logging.INFO)
 
 def foo(s):
     n = int(s)
     # print('>>>n = %d' % n)
-    assert n != 0, 'n is zero!'
+    # assert n != 0, 'n is zero!'
+    logging.info('n = %d' % n)
     return 10 / n
 
 
